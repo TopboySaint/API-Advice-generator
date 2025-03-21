f75a094724de4cc6b5fb0cde480264ff5756e646
TopboySaint <topboysaint1@gmail.com>
2025-03-21 15:17:16 +0100

    Update title and enhance advice saving functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let favoriteAdvice = "";
 const displayAdvice = document.querySelector("#showAdvice");
 
 const getAdvice = async () => {
@@ -6,22 +7,21 @@ const getAdvice = async () => {
   const generateAdvice = await fetch("https://api.adviceslip.com/advice");
   const objectAdvice = await generateAdvice.json();
   // console.log(objectAdvice);
-  displayAdvice.innerHTML = `<p>${objectAdvice.slip.advice}</p>`;
+  favoriteAdvice = objectAdvice.slip.advice;
+
+  displayAdvice.innerHTML = `<p>${favoriteAdvice}</p>`;
 
   let saveBtn = document.createElement("button");
   saveBtn.className =
     "btn btn-danger bg-danger shadow-none form-control text-white fs-5";
   saveBtn.textContent = "Save";
-  saveBtn.onclick = () => saveAdvice(objectAdvice);
+  saveBtn.onclick = () => saveAdvice();
 
   displayAdvice.appendChild(saveBtn);
 };
 
 const saveAdvice = () => {
-  // alert('yes i work')
-  let savedAdvice = localStorage.setItem(
-    "FavoriteAdvice",
-    JSON.stringify(objectAdvice)
-  );
-  console.log(saveAdvice);
+  let favoriteAdvice = displayAdvice.querySelector("p").textContent;
+  localStorage.setItem("FavoriteAdvice", JSON.stringify(favoriteAdvice));
+  console.log("Advice saved:", favoriteAdvice);
 };
